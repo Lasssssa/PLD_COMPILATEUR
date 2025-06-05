e597dbe59e3c49e1834bceca3db0243e797ed568
xonery <kilian.houpeurt@gmail.com>
2025-06-05 09:09:39 +0200

    Add dev container for antlr4

---
+++ build-devcontainer.sh
@@ -0,0 +1,1 @@
+docker build --rm -t ubuntu-antlr4:dev -f Dockerfile .
--- config.mk
+++ config.mk
@@ -1,3 +1,3 @@
-ANTLRJAR=/home/$(USER)/antlr4-install/antlr-4.13.2-complete.jar
+ANTLRJAR=/root/$(USER)/antlr4-install/antlr-4.13.2-complete.jar
 ANTLRINC=/usr/local/include/antlr4-runtime/
 ANTLRLIB=/usr/local/lib/libantlr4-runtime.a
